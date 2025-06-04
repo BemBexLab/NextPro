9e122519fab41642bcc873cdd1172265b8489f74
BemBexLab <dev_admin@bembexlab.com>
2025-06-05 03:26:22 +0500

    feat: redirecting

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,7 @@
-import Demo from '@/components/landingPage/demo'
-import Features from '@/components/landingPage/features'
-import Footer from '@/components/landingPage/footer'
-import Header from '@/components/landingPage/header'
-import Hero from '@/components/landingPage/hero'
-import Loading from '@/components/landingPage/loading'
-import Pricing from '@/components/landingPage/pricing'
-import Technologies from '@/components/landingPage/technologies'
+import { redirect } from "next/navigation";
 
 const Home = () => {
-  return (
-    <>
-      <Header />
-      <Hero />
-      <Technologies />
-      <Features/>
-      <Demo />
-      <Loading/>
-      <Pricing />
-      <Footer />
-    </>
-  )
-}
+  return redirect("/home");
+};
 
-export default Home
+export default Home;
