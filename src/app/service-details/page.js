a10f18fbab51f6a92a36053f9a6281652ae978c2
BemBexLab <dev_admin@bembexlab.com>
2025-06-16 17:40:47 -0700

    adding use client on service-details page

--- src/app/service-details/page.js
+++ src/app/service-details/page.js
@@ -1,3 +1,4 @@
+"use client"
 // /app/service-details/page.jsx
 
 import React from 'react';
@@ -5,10 +6,10 @@ import PageTitle from '@/components/sections/pageTitle';
 import ServiceArtical from '@/components/sections/services/serviceArtical';
 // import ContactForm from '@/components/sections/contactForm'; // Uncomment if needed
 
-export const metadata = {
-  title: "WebFoundersUSA",
-  description: "WebFoundersUSA is a modern Next.js and Tailwind CSS Template that features General Marketing, Social Media Marketing, AI in Marketing, Paid Advertising, Video Marketing, Analytics and Reporting, Industry news & Trends, E-commerce Marketing",
-};
+// export const metadata = {
+//   title: "WebFoundersUSA",
+//   description: "NextPro is a modern Next.js and Tailwind CSS Template that features General Marketing, Social Media Marketing, AI in Marketing, Paid Advertising, Video Marketing, Analytics and Reporting, Industry news & Trends, E-commerce Marketing",
+// };
 
 const ServicesDetails = () => {
   return (
