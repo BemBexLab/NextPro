9c93bea44b1e203e09055aee09f85adb9efbd104
AliKhan17dec <alikhan.dec17@gmail.com>
2026-01-21 22:40:14 +0500

    updting title

--- src/app/service/page.jsx
+++ src/app/service/page.jsx
@@ -1,7 +1,7 @@
 import ServicesSection from "./ServicesSection.client";
 
 export const metadata = {
-  title: "SERVICE  | Web Founders USA",
+  title: "Service  | Web Founders USA",
   description:
     "Explore Web Founders USA services including SEO, web design, and digital marketing solutions built to grow your business.",
     
