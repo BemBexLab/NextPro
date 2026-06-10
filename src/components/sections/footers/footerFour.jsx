445ad2b1ab472446c85079b36741dce44f8d8740
car5p3 <triggred47@icloud.com>
2026-06-10 21:40:24 +0500

    Updated Footer4.jsx All rights reserved part

--- src/components/sections/footers/footerFour.jsx
+++ src/components/sections/footers/footerFour.jsx
@@ -72,7 +72,7 @@ const FooterFour = () => {
                 <div className='max-w-[1350px] mx-auto px-[15px] flex xl:flex-row flex-col justify-between items-center gap-3 py-7.5 text-white'>
                     <p>Designed and Developed By <Link href={""} className='text-primary-foreground font-medium relative hover-underline after:h-[1px]'>WebFounders USA</Link></p>
                     <div className='flex flex-col items-center gap-1 xl:items-end'>
-                        <p>Copyright: © 2025. All Rights Reserved</p>
+                        <p>Copyright: © 2026. All Rights Reserved</p>
                         <Link
                             href="/sitemap.xml"
                             className='text-[10px] absolute font-normal uppercase tracking-[0.08em] text-white/45 transition-all duration-500 hover:text-white/70'
